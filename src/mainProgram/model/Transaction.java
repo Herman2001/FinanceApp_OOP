88cd52da2865db61d030f1cdc5c87a667b0495c9
Herman2001 <102909811+Herman2001@users.noreply.github.com>
2025-12-11 11:42:15 +0100

    add variables to Transaction class

--- src/mainProgram/model/Transaction.java
+++ src/mainProgram/model/Transaction.java
@@ -4,19 +4,36 @@ import java.time.LocalDate;
 import java.time.temporal.WeekFields;
 
 public class Transaction {
+    private final int transactionId;
+    private final int userId;
     private final String description;
     private final double amount;
     private final LocalDate date;
     private final int week;
 
-    public Transaction(String description, double amount, LocalDate date) {
+    // Konstruktor för transaktioner som redan finns i DB
+    public Transaction(int transactionId, int userId, String description, double amount, LocalDate date) {
+        this.transactionId = transactionId;
+        this.userId = userId;
         this.description = description;
         this.amount = amount;
         this.date = date;
+
         WeekFields wf = WeekFields.ISO;
         this.week = date.get(wf.weekOfYear());
     }
 
+    // Konstruktor för nya transaktioner som ska sparas till DB
+    public Transaction(int userId, String description, double amount, LocalDate date) {
+        this(0, userId, description, amount, date);
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+    public int getUserId() {
+        return userId;
+    }
     public String getDescription() { return description; }
     public double getAmount() { return amount; }
     public LocalDate getDate() { return date; }
